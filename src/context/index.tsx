7e96519ebfdeaa3208eee7520c05493984d749e7
iobaidu5 <i.obaidu5@gmail.com>
2023-06-24 18:23:41 +0500

    updated

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -2,5 +2,7 @@ import { createContext } from 'react';
 
 export const ThemeContext = createContext({
   theme: '',
-  setTheme: (theme: string) => {},
+  setTheme: (theme: string) => {
+    console.log(theme)
+  },
 });
